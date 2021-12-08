f200cf6aaa0df302c53c2380c24ec5de062b61ea
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-12-07 22:07:33 -0500

    SQLite DB connected

---
+++ server/db/index.js
@@ -0,0 +1,19 @@
+const sqlite3 = require('sqlite3').verbose();
+
+// open database in memory
+let db = new sqlite3.Database('./mvp2.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the mvp2 SQlite database.');
+});
+
+// close the database connection
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
+
+module.exports.db = db;
